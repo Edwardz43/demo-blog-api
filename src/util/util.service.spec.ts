e3b97d66fef992e560e5b82a6beb40d53bb3c710
Edwardz43 <b93303015@gmail.com>
2022-05-06 20:39:38 +0800

    modify: change encrypt algorithm
    Replace original encrypt algorithm with simple bcrypt hash and add a unit test to check compare method.

--- src/util/util.service.spec.ts
+++ src/util/util.service.spec.ts
@@ -15,4 +15,12 @@ describe('UtilService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+  it('should return the same result', async () => {
+    const password = 'test';
+    const hash = await service.encryptPassword(password).then((res) => res);
+    const result = await service
+      .comparePassword(password, hash)
+      .then((res) => res);
+    expect(result).toBe(true);
+  });
 });
--- src/util/util.service.ts
+++ src/util/util.service.ts
@@ -1,24 +1,16 @@
 import { Injectable } from '@nestjs/common';
-import { createCipheriv, randomBytes, scrypt } from 'crypto';
-import { CreateUserRequest } from '../user/interfaces/interface';
-import { promisify } from 'util';
 import * as bcrypt from 'bcrypt';
 
-const iv = randomBytes(16);
 const saltOrRounds = 10;
 
 @Injectable()
 export class UtilService {
   async encryptPassword(password: string): Promise<string> {
-    const key = (await promisify(scrypt)(password, 'salt', 32)) as Buffer;
-    const cipher = createCipheriv('aes-256-ctr', key, iv);
-
-    const textToEncrypt = 'Nest';
-    const encryptedText = Buffer.concat([
-      cipher.update(textToEncrypt),
-      cipher.final(),
-    ]);
-    const hash = await bcrypt.hash(encryptedText.toString('hex'), saltOrRounds);
+    const hash = await bcrypt.hash(password, saltOrRounds);
     return hash;
+  }
+
+  async comparePassword(password: string, hash: string): Promise<boolean> {
+    return await bcrypt.compare(password, hash);
   }
 }
