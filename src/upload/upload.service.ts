894c0ebc43da2de28d52b464b3da3bc8e5b18462
Edwardz43 <b93303015@gmail.com>
2022-05-11 11:41:46 +0800

    modify: upload method work flow
    Change the file name generating format. It's unique enough and no need to add timestamp as suffix.

--- src/upload/upload.service.ts
+++ src/upload/upload.service.ts
@@ -4,6 +4,8 @@ import * as path from 'path';
 import * as fs from 'fs';
 import { PrismaService } from '../prisma.service';
 
+const urlPrefix = 'http://localhost:3000/upload/';
+
 @Injectable()
 export class UploadService {
   constructor(private readonly prisma: PrismaService) {}
@@ -11,23 +13,30 @@ export class UploadService {
   async upload(userId: number, file: any) {
     const buf = Buffer.from(file);
     const fileName = await this.store(buf);
-    await this.prisma.profile.update({
-      data: {
-        avatar: fileName,
-      },
-      where: {
-        userId,
-      },
-    });
-    const url = 'http://localhost:3000/upload/' + fileName;
+    const ok = await this.prisma.profile
+      .update({
+        data: {
+          avatar: fileName,
+        },
+        where: {
+          userId,
+        },
+      })
+      .catch((error) => {
+        console.error(error.message);
+        return false;
+      });
+    if (!ok) {
+      return 'fail';
+    }
+    const url = urlPrefix + fileName;
     return { url };
   }
 
   async store(image: Buffer): Promise<string> {
     const uniqueId =
       Date.now().toString(36) + Math.random().toString(36).substring(2);
-    const filename = `${uniqueId}-${Date.now()}.jpeg`;
-    console.log(filename);
+    const filename = `${uniqueId}.jpeg`;
     const filepath = path.join(__dirname, filename);
     await fs.writeFileSync(filepath, image);
     return filename;
