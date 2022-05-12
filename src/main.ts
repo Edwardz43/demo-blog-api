529a38535ad18e60312384f4bc09549bf6968991
Edwardz43 <b93303015@gmail.com>
2022-05-12 18:01:59 +0800

    modify: use config module for grpc setting
    Replace client option of grpc setting with config service.

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,26 @@
 import { NestFactory } from '@nestjs/core';
-import { MicroserviceOptions } from '@nestjs/microservices';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
-import { grpcClientOptions } from './grpc-client.options';
 import { ConfigService } from '@nestjs/config';
+import { join } from 'path';
 
 const configService = new ConfigService();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.connectMicroservice<MicroserviceOptions>(grpcClientOptions);
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.GRPC,
+    options: {
+      package: ['user', 'auth', 'post', 'upload'],
+      protoPath: [
+        join(__dirname, './user/user.proto'),
+        join(__dirname, './auth/auth.proto'),
+        join(__dirname, './post/post.proto'),
+        join(__dirname, './upload/upload.proto'),
+      ],
+      url: configService.get<string>('MICROSERVICE_URL'),
+    },
+  });
 
   await app.startAllMicroservices();
   await app.listen(configService.get<number>('port', 3000));
